cb48870568f096373a719ff99f6a67e75d4158e1
alisson galiza <alissongaliza@gmail.com>
2020-10-14 12:13:01 -0300

    delivery: http handler and create user controller

---
+++ src/modules/user/controllers/CreateUserController.ts
@@ -0,0 +1,26 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+import { BaseHttpError } from '../../../shared/infra/http/errors/base_http_error';
+import { InternalServerException } from '../../../shared/infra/http/errors/http_server_error';
+import { HttpRequestService } from '../../../shared/infra/http/helpers/requestHandler';
+import { CreateUserDTO } from '../entities/UserDTO';
+import { userSchema } from '../entities/UserSchema';
+import { CreateUserRepository } from '../repository/concrete/dynamodb/CreateUserRepository';
+import { CreateUserUsecase } from '../usecase/concrete/CreateUserUsecase';
+
+const createUserRepository = new CreateUserRepository();
+const createUserUsecase = new CreateUserUsecase(createUserRepository);
+
+export const addUser: APIGatewayProxyHandler = async (event): Promise<any> => {
+	const requestHandler = new HttpRequestService();
+	try {
+		const body: CreateUserDTO = await requestHandler.validateBody(event.body, userSchema);
+		const created = await createUserUsecase.create(body);
+		if (created) {
+			requestHandler.success({}, 201);
+		}
+	} catch (error) {
+		if (error instanceof BaseHttpError) return requestHandler.error(error);
+		return requestHandler.error(new InternalServerException(error));
+	}
+};
---
+++ src/shared/infra/http/helpers/requestHandler.ts
@@ -0,0 +1,70 @@
+import Joi from 'joi';
+
+import { BaseHttpError } from '../errors/base_http_error';
+import { InvalidJsonException, InvalidParamsException, NullBodyException } from '../errors/http_client_error';
+
+const headers = { 'Access-Control-Allow-Origin': '*' };
+
+/**
+ * Implementation of the request service for HTTP protocol.
+ */
+export class HttpRequestService {
+	validateBody(body: any, validator: Joi.ObjectSchema<any>): Promise<any> {
+		return new Promise((resolve) => {
+			if (body === null) {
+				throw new NullBodyException();
+			}
+			try {
+				body = JSON.parse(body);
+			} catch {
+				throw new InvalidJsonException(body);
+			}
+			const validationResult = validator.validate(body);
+			if (validationResult.errors) throw new InvalidParamsException(validationResult.errors);
+
+			resolve(body);
+		});
+	}
+
+	success(body: any = {}, httpStatus = 200, message = 'HTTP_SUCCESS_DEFAULT'): Promise<any> {
+		return Promise.resolve({
+			statusCode: httpStatus,
+			headers: headers,
+			body: JSON.stringify({
+				message: message,
+				data: body,
+			}),
+		});
+	}
+
+	error(error: BaseHttpError): Promise<any> {
+		return Promise.resolve({
+			headers: headers,
+			statusCode: error.httpStatus,
+			body: JSON.stringify({
+				message: error.message,
+				errorCode: error.errorCode,
+				data: error.data,
+			}),
+		});
+	}
+
+	successPaginate(
+		body: any = {},
+		total: number,
+		lastIndex: string,
+		httpStatus = 200,
+		message = 'HTTP_SUCCESS_DEFAULT'
+	): Promise<any> {
+		return Promise.resolve({
+			statusCode: httpStatus,
+			headers: headers,
+			body: JSON.stringify({
+				message: message,
+				total: total,
+				lastEvaluatedIndex: lastIndex,
+				data: body,
+			}),
+		});
+	}
+}
