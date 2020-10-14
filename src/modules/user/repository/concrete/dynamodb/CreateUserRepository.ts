6d9c49d20443c831969ca44e7cc45e443f936d38
alisson galiza <alissongaliza@gmail.com>
2020-10-14 16:32:07 -0300

    repository: create user now returns the autogenerated uuid

--- src/modules/user/repository/concrete/dynamodb/CreateUserRepository.ts
+++ src/modules/user/repository/concrete/dynamodb/CreateUserRepository.ts
@@ -2,23 +2,28 @@ import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 import * as uuid from 'uuid';
 
 import { DynamoDBInstance } from '../../../../../shared/infra/dynamodb/DynamoDBInstance';
-import { User } from '../../../entities/User';
+import { DBException } from '../../../../../shared/infra/dynamodb/errors/DBException';
 import { CreateUserDTO } from '../../../entities/UserDTO';
 import { ICreateUserRepository } from '../../interface/ICreateUserRepository';
 
 export class CreateUserRepository implements ICreateUserRepository {
-	async create(data: CreateUserDTO): Promise<User> {
+	async create(data: CreateUserDTO): Promise<string> {
+		const id = uuid.v4();
 		const formattedUser = {
-			entity: `USER#${uuid.v4()}`,
-			data: `METADATA#${data.name}`,
+			entity: `USER#${id}`,
+			data: `METADATA#${id}`,
 			favoriteThemes: data.favoriteThemes,
 		};
 		const params: DocumentClient.PutItemInput = {
 			TableName: process.env.DB_TABLE || '',
 			Item: formattedUser,
 		};
-		const added: any = await DynamoDBInstance.put(params).promise();
-		console.log({ added });
-		return added;
+		try {
+			const added: any = await DynamoDBInstance.put(params).promise();
+			// return only the uuid generated part
+			return formattedUser.entity.split('#')[1];
+		} catch (e) {
+			throw new DBException(e.message, e.statusCode);
+		}
 	}
 }
