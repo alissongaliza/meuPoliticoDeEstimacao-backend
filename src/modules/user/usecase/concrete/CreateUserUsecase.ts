64b5c8b865840d15b1bd31c260d7d126a54a3dcd
alisson galiza <alissongaliza@gmail.com>
2020-10-14 16:37:27 -0300

    usecase: finishing createuser flow

--- src/modules/user/usecase/concrete/CreateUserUsecase.ts
+++ src/modules/user/usecase/concrete/CreateUserUsecase.ts
@@ -1,12 +1,17 @@
 import { User } from '../../entities/User';
 import { CreateUserDTO } from '../../entities/UserDTO';
 import { ICreateUserRepository } from '../../repository/interface/ICreateUserRepository';
+import { IGetByIdUserRepository } from '../../repository/interface/IGetByIdUserRepository';
 import { ICreateUserUsecase } from '../interface/ICreateUserUsecase';
 
 export class CreateUserUsecase implements ICreateUserUsecase {
-	constructor(private readonly createUserRepository: ICreateUserRepository) {}
+	constructor(
+		private readonly createUserRepository: ICreateUserRepository,
+		private readonly getByIdUserRepository: IGetByIdUserRepository
+	) {}
 
 	async create(data: CreateUserDTO): Promise<User> {
-		return await this.createUserRepository.create(data);
+		const generatedId = await this.createUserRepository.create(data);
+		return await this.getByIdUserRepository.getById(generatedId);
 	}
 }
