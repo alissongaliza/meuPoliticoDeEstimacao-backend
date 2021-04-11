2f59e3fbc6ecb9fe5e7587314c4e57d38d9ec3e7
Alisson Galiza <alisson.galiza@gmail.com>
2021-04-11 17:07:07 -0300

    refactor: simplifying usecase interfaces

--- src/modules/user/usecase/interface/ICreateUserUsecase.ts
+++
@@ -1,5 +0,0 @@
-import { CreateUserDTO } from '../../entities/UserDTO';
-
-export interface ICreateUserUsecase {
-	create(data: CreateUserDTO): Promise<boolean>;
-}
--- src/modules/user/usecase/interface/IGetUserUsecase.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '../../entities/User';
-
-export interface IGetUserUsecase {
-	get(name: string): Promise<User>;
-}
