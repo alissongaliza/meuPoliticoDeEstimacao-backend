579e9e10374a903f9c7ce1141bcadee1ba04c254
Alisson Galiza <alissongaliza@gmail.com>
2021-05-08 21:40:09 -0300

    refactor: script can now generate a friendlier seed for visual lookup

--- src/shared/util/generateSeed.js
+++ src/shared/util/generateSeed.js
@@ -3,120 +3,107 @@
 
 require('dotenv').config();
 const _range = require('lodash/range');
-const _groupBy = require('lodash/groupBy');
 const _flatten = require('lodash/flatten');
 const fetch = require('node-fetch');
 
-const earliestYear = 2019;
+const earliestYear = 2020;
 const latestYear = 2021;
+const fullSeedPath = `${process.env.BASE_PROJECT_PATH}/src/shared/infra/dynamodb/newSeed.json`;
+const miniSeedPath = `${process.env.BASE_PROJECT_PATH}/src/shared/infra/dynamodb/miniSeed.json`;
+const validPropositionsHash = {};
 
-const getData = async () => {
-	const validPropositionsHash = {};
-	const data = _range(earliestYear, latestYear).map((year) => {
-		let { dados: propositions } = require(`${process.env.SOURCE_FILES_PATH}/proposicoes-${year}`);
-		propositions = propositions
-			.filter((el) => el.codTipo == '136' || el.codTipo == '139')
-			.map((proposition) => {
-				const { uri = '', ementa = '', keywords = [], id } = proposition;
-				validPropositionsHash[id + ''] = [];
-				const rawNonEmptyTags = keywords.split(',').filter(Boolean);
-				const tags = rawNonEmptyTags.map((el) => el.trim());
-				if (tags.length > 0) tags[tags.length - 1] = tags[tags.length - 1].substring(0, tags.length - 1);
-				return {
-					PK: `PROPOSITION#${id}`,
-					SK: `PROPOSITION#${id}`,
-					uri,
-					ementa,
-					id,
-					tags,
-				};
-			});
+function getPropositions(year) {
+	let { dados: propositions } = require(`${process.env.SOURCE_FILES_PATH}/proposicoes-${year}`);
+	return propositions
+		.filter((el) => el.codTipo == '136' || el.codTipo == '139')
+		.map((proposition) => {
+			const { uri = '', ementa = '', keywords = [], id } = proposition;
+			validPropositionsHash[id + ''] = [];
+			const rawNonEmptyTags = keywords.split(',').filter(Boolean);
+			const tags = rawNonEmptyTags.map((el) => el.trim());
+			if (tags.length > 0) tags[tags.length - 1] = tags[tags.length - 1].substring(0, tags.length - 1);
+			return {
+				PK: `PROPOSITION#${id}`,
+				SK: `PROPOSITION#${id}`,
+				uri,
+				ementa,
+				id,
+				tags,
+			};
+		});
+}
 
-		let { dados: propositionsAndThemes } = require(`${process.env.SOURCE_FILES_PATH}/proposicoesTemas-${year}`);
-		propositionsAndThemes = propositionsAndThemes
-			.filter(({ uriProposicao }) => !!validPropositionsHash[uriProposicao.split('proposicoes/')[1] + ''])
-			.map(({ codTema, uriProposicao }) => {
-				const propositionId = uriProposicao.split('proposicoes/')[1];
-				validPropositionsHash[propositionId + ''].push(codTema + '');
-				return {
-					PK: `PROPOSITION#${propositionId}`,
-					SK: `THEME#${codTema}`,
-					GSI1PK: `THEME#${codTema}`,
-					GSI1SK: `#PROPOSITION#${propositionId}`,
-					themeId: codTema + '',
-					propositionId,
-				};
-			});
+function getPropositionsAndThemes(year) {
+	let { dados: propositionsAndThemes } = require(`${process.env.SOURCE_FILES_PATH}/proposicoesTemas-${year}`);
+	return propositionsAndThemes
+		.filter(({ uriProposicao }) => !!validPropositionsHash[uriProposicao.split('proposicoes/')[1] + ''])
+		.map(({ codTema, uriProposicao }) => {
+			const propositionId = uriProposicao.split('proposicoes/')[1];
+			validPropositionsHash[propositionId + ''].push(codTema + '');
+			return {
+				PK: `PROPOSITION#${propositionId}`,
+				SK: `THEME#${codTema}`,
+				GSI1PK: `THEME#${codTema}`,
+				GSI1SK: `#PROPOSITION#${propositionId}`,
+				themeId: codTema + '',
+				propositionId,
+			};
+		});
+}
 
-		const { dados: propositionsAuthors } = require(`${process.env.SOURCE_FILES_PATH}/proposicoesAutores-${year}`);
-		const authorsAndPropositions = propositionsAuthors
-			.filter((el) => el.idDeputadoAutor !== undefined && !!validPropositionsHash[el.idProposicao + ''])
-			.map(({ idProposicao, idDeputadoAutor }) => ({
-				PK: `PROPOSITION#${idProposicao}`,
-				SK: `POLITICIAN#${idDeputadoAutor}`,
-				politicianId: idProposicao + '',
-				propositionId: idDeputadoAutor + '',
-			}));
+function getAuthorsPropositions(year) {
+	const { dados: propositionsAuthors } = require(`${process.env.SOURCE_FILES_PATH}/proposicoesAutores-${year}`);
+	return propositionsAuthors
+		.filter((el) => el.idDeputadoAutor !== undefined && !!validPropositionsHash[el.idProposicao + ''])
+		.map(({ idProposicao, idDeputadoAutor }) => ({
+			PK: `PROPOSITION#${idProposicao}`,
+			SK: `POLITICIAN#${idDeputadoAutor}`,
+			politicianId: idDeputadoAutor + '',
+			propositionId: idProposicao + '',
+		}));
+}
 
-		const authorsThemesTrackHash = {};
-		propositionsAuthors
-			.filter((el) => el.idDeputadoAutor !== undefined && !!validPropositionsHash[el.idProposicao + ''])
-			.map(({ idProposicao, idDeputadoAutor }) => {
-				const themes = validPropositionsHash[idProposicao + ''] || [];
-				return themes.map((themeId) => {
-					const key = `THEME#${themeId}POLITICIAN#${idDeputadoAutor}`;
-					const count = (authorsThemesTrackHash[key] && authorsThemesTrackHash[key].count + 1) || 1;
-					const paddedCount = count.toString().padStart(5, '0');
-					authorsThemesTrackHash[key] = {
-						PK: `THEME#${themeId}`,
-						SK: `POLITICIAN#${idDeputadoAutor}`,
-						themeId,
-						authorId: idDeputadoAutor,
-						count,
-						GS1PK: `THEME#${themeId}`,
-						GS1SK: `${paddedCount}POLITICIAN#${idDeputadoAutor}`,
-					};
-				});
-			});
-
-		const authorsAndThemes = Object.values(authorsThemesTrackHash);
-
-		return [...propositions, ...propositionsAndThemes, ...authorsAndPropositions, ...authorsAndThemes];
+function getAuthorsThemesTrackHash(propositionsAuthors) {
+	const authorsThemesTrackHash = {};
+	propositionsAuthors.map(({ propositionId, politicianId }) => {
+		const themes = validPropositionsHash[propositionId + ''] || [];
+		return themes.map((themeId) => {
+			const key = `THEME#${themeId}POLITICIAN#${politicianId}`;
+			const count = (authorsThemesTrackHash[key] && authorsThemesTrackHash[key].count + 1) || 1;
+			const paddedCount = count.toString().padStart(5, '0');
+			authorsThemesTrackHash[key] = {
+				PK: `THEME#${themeId}`,
+				SK: `POLITICIAN#${politicianId}`,
+				themeId,
+				authorId: politicianId,
+				count,
+				GSI1PK: `THEME#${themeId}`,
+				GSI1SK: `z${paddedCount}POLITICIAN#${politicianId}`,
+			};
+		});
 	});
+	return authorsThemesTrackHash;
+}
 
-	const flattenData = _flatten(data);
-
-	let themes = await getThemes();
+function getAllThemes(themes) {
 	let allThemes = themes.reduce((acc, { cod: id, nome: name }) => {
 		return { ...acc, [id]: { id, name, count: 0 } };
 	}, {});
-	allThemes = { themes: { ...allThemes }, PK: 'THEMES', SK: 'THEMES' };
-	themes
-		.map((theme) => {
-			const { cod, nome: name } = theme;
-			return {
-				PK: `THEME#${cod}`,
-				SK: `THEME#${cod}`,
-				name,
-				GSI1PK: `THEME#${cod}`,
-				GSI1SK: `THEME#${cod}`,
-				id: cod,
-			};
-		})
-		.map((el) => flattenData.push(el));
-	flattenData.push(allThemes);
+	return { themes: { ...allThemes }, PK: 'THEMES', SK: 'THEMES' };
+}
 
-	const politicians = await getPoliticians();
-	politicians.map((el) => flattenData.push(el));
-
-	writeToFile(flattenData);
-};
-
-async function getThemes() {
-	const response = await fetch(
-		'https://dadosabertos.camara.leg.br/api/v2/referencias/proposicoes/codTema'
-	).then((response) => response.json());
-	return response.dados;
+function getFormattedThemes(themes) {
+	return themes.map((theme) => {
+		const { cod, nome: name } = theme;
+		return {
+			PK: `THEME#${cod}`,
+			SK: `THEME#${cod}`,
+			name,
+			GSI1PK: `THEME#${cod}`,
+			GSI1SK: `THEME#${cod}`,
+			id: cod,
+		};
+	});
 }
 
 async function getPoliticians() {
@@ -167,19 +154,61 @@ async function getPoliticians() {
 	});
 }
 
-function writeToFile(data) {
+async function getThemes() {
+	const response = await fetch(
+		'https://dadosabertos.camara.leg.br/api/v2/referencias/proposicoes/codTema'
+	).then((response) => response.json());
+	return response.dados;
+}
+
+const getAllData = async () => {
+	const data = _range(earliestYear, latestYear).map((year) => {
+		const propositions = getPropositions(year);
+
+		const propositionsAndThemes = getPropositionsAndThemes(year);
+
+		const authorsPropositions = getAuthorsPropositions(year);
+
+		const authorsThemesTrackHash = getAuthorsThemesTrackHash(authorsPropositions);
+
+		const authorsAndThemes = Object.values(authorsThemesTrackHash);
+
+		return [propositions, propositionsAndThemes, authorsPropositions, authorsAndThemes];
+	});
+
+	const flattenData = _flatten(data);
+	const themes = await getThemes();
+	const allThemes = getAllThemes(themes);
+	flattenData.push([allThemes]);
+	const formattedThemes = getFormattedThemes(themes);
+	flattenData.push(formattedThemes);
+	const politicians = await getPoliticians();
+	flattenData.push(politicians);
+	return flattenData;
+};
+async function generateFullSeed() {
+	const data = await getAllData();
+	const flattenData = _flatten(data);
+	writeToFile(flattenData, fullSeedPath);
+}
+
+async function generateMiniSeed() {
+	const data = await getAllData();
+	const miniData = data.map((el) => el.slice(0, 5));
+	const miniFlattenData = _flatten(miniData);
+	writeToFile(miniFlattenData, miniSeedPath);
+}
+
+function writeToFile(data, path) {
 	const fs = require('fs');
 	try {
-		fs.writeFile(
-			`${process.env.BASE_PROJECT_PATH}/src/shared/infra/dynamodb/newSeed.json`,
-			JSON.stringify(data, null, 4),
-			(err, result) => {
-				if (err) console.info(err);
-			}
-		);
+		fs.writeFile(path, JSON.stringify(data, null, 4), (err, result) => {
+			if (err) console.info(err);
+		});
 	} catch (err) {
 		console.error(err);
 	}
 }
 
-getData();
+generateFullSeed();
+generateMiniSeed();
